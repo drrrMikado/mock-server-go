cb7d4bad7cd736398ce4fd78b86cb4b84316f7a3
drrrMikado <liliang.illusion@hotmail.com>
2020-05-14 15:41:57 +0800

    Change mock struct.

--- models/model.go
+++ models/model.go
@@ -1,16 +1,41 @@
 package models
 
 import (
-	"github.com/drrrMikado/mock-server-go/internal/time"
+	"database/sql/driver"
+	"encoding/json"
 )
 
 type Mock struct {
-	ID         string            `json:"id,omitempty"`
-	Desc       string            `json:"description"`
-	Uri        string            `json:"uri"`
-	Method     string            `json:"method"`
-	Delay      time.Duration     `json:"delay"`
-	StatusCode int               `json:"status_code"`
-	Headers    map[string]string `json:"headers"`
-	Body       string            `json:"body"`
+	ID          uint64          `json:"id"`
+	Description string          `json:"description"`
+	Uri         string          `json:"uri"`
+	Method      string          `json:"method"`
+	Delay       string          `json:"delay"`
+	StatusCode  int             `json:"status_code"`
+	Headers     Header          `json:"headers"`
+	Body        json.RawMessage `json:"body"`
+}
+
+func (*Mock) TableName() string {
+	return "mock"
+}
+
+type Header map[string]string
+
+func (h *Header) Value() (driver.Value, error) {
+	b, err := json.Marshal(h)
+	if err != nil {
+		return nil, err
+	}
+	return string(b), nil
+}
+
+func (h *Header) Scan(src interface{}) (err error) {
+	switch v := src.(type) {
+	case []uint8:
+		if err = json.Unmarshal(v, &h); err != nil {
+			return err
+		}
+	}
+	return
 }
