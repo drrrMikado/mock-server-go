1ab7d5284145edc77ddd2fed6503dc559d9642dd
drrrMikado <liliang.illusion@hotmail.com>
2020-05-15 15:49:32 +0800

    Add test case.

--- handlers/admin_test.go
+++ handlers/admin_test.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"bytes"
+	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -47,4 +49,48 @@ func TestUpdateWithErrorID(t *testing.T) {
 	})
 	w := performRequest(router, "POST", "/test/update/error/-1")
 	assert.Equal(t, http.StatusBadRequest, w.Code)
+}
+
+func TestAddOrUpdateWithEmptyField(t *testing.T) {
+	gin.DefaultWriter = os.Stdout
+	gin.SetMode(gin.TestMode)
+	router := gin.New()
+	router.POST("/test/add/empty/field", func(c *gin.Context) {
+		Add(c)
+		return
+	})
+	router.POST("/test/update/empty/field", func(c *gin.Context) {
+		Update(c)
+		return
+	})
+	boundary := "--testBoundary"
+	body := new(bytes.Buffer)
+	mw := multipart.NewWriter(body)
+	defer func() {
+		_ = mw.Close()
+	}()
+
+	assert.NoError(t, mw.SetBoundary(boundary))
+	assert.NoError(t, mw.WriteField("description", "description"))
+	assert.NoError(t, mw.WriteField("uri", ""))
+	assert.NoError(t, mw.WriteField("method", ""))
+	assert.NoError(t, mw.WriteField("delay", "20ms"))
+	assert.NoError(t, mw.WriteField("status_code", "200"))
+	assert.NoError(t, mw.WriteField("headers", ""))
+	assert.NoError(t, mw.WriteField("body", "{}"))
+	// Test Add
+	req, err := http.NewRequest("POST", "/test/add/empty/field", body)
+	assert.NoError(t, err)
+	req.Header.Set("Content-Type", gin.MIMEMultipartPOSTForm+"; boundary="+boundary)
+	w1 := httptest.NewRecorder()
+	router.ServeHTTP(w1, req)
+	assert.Equal(t, http.StatusBadRequest, w1.Code)
+	// Test Update
+	assert.NoError(t, mw.WriteField("id", ""))
+	req, err = http.NewRequest("POST", "/test/update/empty/field", body)
+	assert.NoError(t, err)
+	req.Header.Set("Content-Type", gin.MIMEMultipartPOSTForm+"; boundary="+boundary)
+	w2 := httptest.NewRecorder()
+	router.ServeHTTP(w2, req)
+	assert.Equal(t, http.StatusBadRequest, w2.Code)
 }
